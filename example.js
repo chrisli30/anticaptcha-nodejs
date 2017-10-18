119c3bd425906f06baeb59e0c120a132afe8ce36
Chris Li <chris.li.2046@gmail.com>
2017-10-18 01:43:59 -0700

    Tweaked example

--- example.js
+++ example.js
@@ -3,18 +3,18 @@
 
 // Params:
 // your anti-captcha.com account key
-var anticaptcha = require('./anticaptcha')('12345678901234567890123456789012');
+var anticaptcha = require('./anticaptcha')('1b61c6ecba16e325cf0f8ff845e06e52');
 
 //recaptcha key from target website
-anticaptcha.setWebsiteURL("http://mywebsite.com/recaptcha/test.php");
-anticaptcha.setWebsiteKey("sitekey-can-be-taken-from-div.g-recaptcha-element");
+anticaptcha.setWebsiteURL("http://www.authorship.com/withdraw");
+anticaptcha.setWebsiteKey("r20170915175810");
 
 //proxy access parameters
-anticaptcha.setProxyType("http");
-anticaptcha.setProxyAddress("proxyaddress");
-anticaptcha.setProxyPort(8080);
-anticaptcha.setProxyLogin("proxylogin");
-anticaptcha.setProxyPassword("proxypassword");
+// anticaptcha.setProxyType("http");
+// anticaptcha.setProxyAddress("proxyaddress");
+// anticaptcha.setProxyPort(8080);
+// anticaptcha.setProxyLogin("proxylogin");
+// anticaptcha.setProxyPassword("proxypassword");
 
 //browser header parameters
 anticaptcha.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116");
